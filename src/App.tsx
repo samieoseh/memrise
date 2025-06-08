a5cba84a972c6910a2b097a65092e834697e9c78
Samuel Oseh <samieoseh@gmail.com>
2025-06-08 11:01:55 +0100

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+    </div>
+  );
 }
